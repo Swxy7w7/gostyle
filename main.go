85e087c632ace36615406887674608e56b46b9cd
&! Tekky#1337 <98614666+xtekky@users.noreply.github.com>
2022-08-22 06:02:34 +0200

    Update main.go

--- main.go
+++ main.go
@@ -128,11 +128,11 @@ func StaticRGB(r int, g int, b int) string {
 	}())
 }
 
-func Symbol(symbol string, col string, col_left_right string, message string) string {
+func Symbol(symbol, col, col_left_right, message_color, message string) string {
 
 	return fmt.Sprintf(
-		"%s%s%s%s%s%s%s",
-		col_left_right, "[", col, symbol, col_left_right, "]", reset,
+		"%s%s%s%s%s%s%s%s%s",
+		col_left_right, "[", col, symbol, col_left_right, "] ", message_color, message, reset,
 	)
 }
 
